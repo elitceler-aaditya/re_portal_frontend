1133810feb854f07b6eac39bb8d6eb07ab511791
Aaditya Jagdale <aadityajagdale.21@gmail.com>
2024-08-27 13:42:35 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/re_portal_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.re_portal_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:re_portal_frontend/modules/home/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Reportal Tech',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
